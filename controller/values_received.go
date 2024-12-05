5b03d74f8845f5ad1e3f7e4e0ca65e9235982a68
MorganaDuart <morgana.duarte@saleshunter.ai>
2024-12-05 15:26:24 -0300

    save value

--- controller/values_received.go
+++ controller/values_received.go
@@ -2,33 +2,45 @@ package controller
 
 import (
 	"encoding/json"
+	"expense-manager/resource"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 type ValuesReceivedInput struct {
-	ValueReceived       int    `json:"value_received"`
-	DateReceived        string `json:"date_received"`
-	DescriptionReceived string `json:"description_received"`
-	AccountReceived     string `json:"account_received"`
+	ValueReceived       float32 `json:"value_received"`
+	DateReceived        string  `json:"date_received"`
+	DescriptionReceived string  `json:"description_received"`
+	AccountReceived     string  `json:"account_received"`
 }
 
 type ValuesReceivedResponse struct {
-	Value       int    `json:"value"`
-	Date        string `json:"date"`
-	Description string `json:"description"`
-	Account     string `json:"account"`
+	Value       float32 `json:"value"`
+	Date        string  `json:"date"`
+	Description string  `json:"description"`
+	Account     string  `json:"account"`
 }
 
 func ValuesReceived(w http.ResponseWriter, r *http.Request) {
 	var valueInput ValuesReceivedInput
 
+	fmt.Println(valueInput)
 	if err := json.NewDecoder(r.Body).Decode(&valueInput); err != nil {
 		log.Println("Error decoding JSON:", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
+	dateReceived, err := time.Parse("2006-01-02", valueInput.DateReceived)
+	if err != nil {
+		fmt.Println("Erro ao converter a string em data:", err)
+		return
+	}
+
+	database := resource.GetDatabaseInstance()
+	database.SaveValueReceived(valueInput.ValueReceived, dateReceived, valueInput.DescriptionReceived, valueInput.AccountReceived)
 	w.Header().Set("Content-Type", "application/json")
 	response := ValuesReceivedResponse{
 		Value:       valueInput.ValueReceived,
